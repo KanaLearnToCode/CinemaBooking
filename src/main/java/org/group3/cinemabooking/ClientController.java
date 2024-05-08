de3d485641f7f3c7cf4ed0052fc210dad9379419
Dell <trinhhoanvu25041998@gmail.com>
2024-05-08 09:42:28 +0700

    Update LoginController

--- src/main/java/org/group3/cinemabooking/ClientController.java
+++ src/main/java/org/group3/cinemabooking/ClientController.java
@@ -1,4 +1,13 @@
 package org.group3.cinemabooking;
 
-public class ClientController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println(LoginController.getLoggedInUser().getEmail());
+    }
 }
--- src/main/java/org/group3/cinemabooking/LoginController.java
+++ src/main/java/org/group3/cinemabooking/LoginController.java
@@ -17,6 +17,8 @@ import java.util.logging.Logger;
 
 public class LoginController implements Initializable {
 
+    private static Account loggedInUser;
+
     @FXML
     private Text emailError;
 
@@ -31,65 +33,46 @@ public class LoginController implements Initializable {
 
     @FXML
     void onLogin(ActionEvent event) {
+        emailError.setText("");
+        passErr.setText("");
+
+        String userName = txEmail.getText();
+        String password = txPass.getText();
+
         EntityManagerFactory entityManagerFactory = null;
         EntityManager entityManager = null;
         EntityTransaction entityTransaction = null;
 
+        if (userName.isBlank()) {
+            emailError.setText("Invalid email or password");
+        }
+        if (password.isBlank()) {
+            passErr.setText("Invalid email or password");
+        }
         try {
             entityManagerFactory = Persistence.createEntityManagerFactory("default");
             entityManager = entityManagerFactory.createEntityManager();
             entityTransaction = entityManager.getTransaction();
             entityTransaction.begin();
-
-            String userName = txEmail.getText();
-            String password = txPass.getText();
-
-            if (userName.isBlank() || password.isBlank()) {
-                emailError.setText("Invalid email or password");
-                passErr.setText("Invalid email or password");
-                if (entityTransaction.isActive()) {
-                    entityTransaction.rollback();
-                }
-                entityManager.close();
-                entityManagerFactory.close();
-                return;
+            Account account = (Account) entityManager.createQuery("select a from Account a where a.email = :email and a.password = :password")
+                    .setParameter("email", userName).setParameter("password", password)
+                    .getSingleResult();
+            loggedInUser = account;
+            if (account.getRole()) {
+                App.setRoot("Admin", "Admin Controller");
+            } else {
+                App.setRoot("Client", "Client Controller");
             }
-            try {
-                Account account = (Account) entityManager.createQuery("select a from Account a where a.email = :email and a.password = :password")
-                        .setParameter("email", userName).setParameter("password", password)
-                        .getSingleResult();
-                if (account.getRole()) {
-                    App.setRoot("Admin", "Admin Controller");
-                } else {
-                    App.setRoot("Client", "Client Controller");
-                }
-            } catch (NoResultException e) {
-                emailError.setText("Invalid email or password");
-                passErr.setText("Invalid email or password");
-                return;
-            } finally {
-                if (entityTransaction.isActive()) {
-                    entityTransaction.rollback();
-                }
-                entityManager.close();
-                entityManagerFactory.close();
-            }
-
             entityTransaction.commit();
         } catch (Exception e) {
-            // Nếu có lỗi xảy ra, rollback giao dịch
-            if (entityTransaction != null && entityTransaction.isActive()) {
-                entityTransaction.rollback();
-            }
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);
         } finally {
-            // Đóng EntityManager và EntityManagerFactory khi không cần thiết nữa
-            if (entityManager != null) {
-                entityManager.close();
+            assert entityTransaction != null;
+            if (entityTransaction.isActive()) {
+                entityTransaction.rollback();
             }
-            if (entityManagerFactory != null) {
-                entityManagerFactory.close();
-            }
+            entityManager.close();
+            entityManagerFactory.close();
         }
     }
 
@@ -107,5 +90,9 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         txEmail.getStyleClass().add("emailLogin");
         txPass.getStyleClass().add("passLogin");
+    }
+
+    public static Account getLoggedInUser() {
+        return loggedInUser;
     }
 }
